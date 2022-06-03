55b125587a5ef19b791f378af2eaed187f99b6c2
GianlucaG00 <gianlu.gambi@gmail.com>
2022-06-03 22:37:38 +0200

    finito api cocktail

--- app/javascript/packs/cocktails/search_api.js
+++ app/javascript/packs/cocktails/search_api.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
-    $("#search").on("click", function() {
-        value = $("#text-field").text(); 
-        if(value == ""){
-            alert("Inserisci un cocktail da cercare!"); 
-            return; 
+    $("#search-cocktail").on("click", function() {
+        text = $("#text-field").val(); 
+        if(text.length === 0){
+            alert("Inserisci il nome di un cocktail da cercare!"); 
+            return false; 
         }
     });
+
+    $("#add-button").on("click", function() {
+        drink_name = $("#drink-name-api").text();
+        drink_id = $("#drink-id-api").text(); 
+        drink_preparation = $("#drink-preparation-api").text(); 
+         
+        $("#cocktail-name").val(drink_name); 
+        $("#cocktail-preparation").val(drink_preparation); 
+    }); 
 });
+
+
