02e26f32a1d5be220123a97558ba6d88baa02d14
GianlucaG00 <gianlu.gambi@gmail.com>
2022-06-04 00:22:45 +0200

    Inizio Area Personale Barman

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_01_203346) do
+ActiveRecord::Schema.define(version: 2022_06_03_212724) do
 
   create_table "barmen", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2022_06_01_203346) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "provider"
     t.string "uid"
+    t.string "name"
+    t.string "surname"
     t.index ["email"], name: "index_barmen_on_email", unique: true
     t.index ["reset_password_token"], name: "index_barmen_on_reset_password_token", unique: true
   end
